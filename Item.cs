0ae75ed79ed1fd79cde36618d408c4d0becd21d2
Nagy_Mate <112873526+nmate0814@users.noreply.github.com>
2024-05-05 16:16:01 +0200

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YT6AIQ_Beadando
+{
+    internal class Item
+    {
+        //Változók, getter, setter
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        //Konstruktor
+        public Item(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        // Az Equals metódust felüldefiniáljuk, hogy megnézzük van-e egyezőség
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Item other = (Item)obj;
+            return ID == other.ID && Name == other.Name;
+        }
+
+        // A GetHashCode metódus felüldifiniáláda a hashkód generáláshoz
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YT6AIQ_Beadando
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HashSet példányosítása
+            HashSet<Item> items = new HashSet<Item>();
+
+            //bevásárló lista feltöltése 20 elemmel
+            items.Add(new Item(1, "Alma"));
+            items.Add(new Item(2, "Méz"));
+            items.Add(new Item(3, "Rizs"));
+            items.Add(new Item(4, "Kenyér"));
+            items.Add(new Item(5, "Zsemle"));
+            items.Add(new Item(6, "Liszt"));
+            items.Add(new Item(7, "Cukor"));
+            items.Add(new Item(8, "Ecet"));
+            items.Add(new Item(9, "Tejföl"));
+            items.Add(new Item(10, "Tojás"));
+            items.Add(new Item(11, "Tészta"));
+            items.Add(new Item(12, "Csirkecomb"));
+            items.Add(new Item(13, "Sertéscomb"));
+            items.Add(new Item(14, "Darálthús"));
+            items.Add(new Item(15, "Csipsz"));
+            items.Add(new Item(16, "Lekvár"));
+            items.Add(new Item(17, "Kakaó"));
+            items.Add(new Item(18, "Kóla"));
+            items.Add(new Item(19, "Kávé"));
+            items.Add(new Item(20, "Tea"));
+
+            //Ki listázza a bevásárló darabszámát
+            Console.WriteLine("Hány tételt tartalmaz a bevásárló lista: " + items.Count);
+
+            //Megnézi hogy van-e már benne kenyér
+            Console.WriteLine("Van-e benne kenyér?: " + items.Contains(new Item(4, "Kenyér")));
+
+            //Kiveszi a mézet a bevásárló listából
+            Console.WriteLine("Ki vette-e a mézet: " + items.Remove(new Item(2, "Méz")));
+
+            //A méz eltávolítása után megszámolja a bevásárló listán lévő cikkeket
+            Console.WriteLine("A méz eltávolítása után: " + items.Count + "db\n");
+            
+            //Bevásárló lista kiírása
+            Console.WriteLine("Bevásárló lista:");
+            foreach (var item in items)
+            {
+                Console.WriteLine("ID: " + item.ID + ", Name: " + item.Name);
+            }
+
+            //Egy példa string, amin műveletet/formázást végzünk
+            string exampleString = "Ez egy bevásárló lista!";
+
+            //Megmutatja a szöveget
+            Console.WriteLine("\nEredeti szöveg: " + exampleString);
+            
+            //Kiírja a szöveget nagybetűkkel
+            Console.WriteLine("Nagybetűvel: " + exampleString.ToUpper());
+
+            //Kiírja a szöveg hosszát
+            Console.WriteLine("Hossza: " + exampleString.Length + " karakter");
+
+            //Szöveg egy részletének kiírása (3. karaktertől 13 karakterig)
+            Console.WriteLine("Szövegrészlet: " + exampleString.Substring(3, 13) + " (3. tól a 13.-ig karakterig)");
+        }
+    }
+}
